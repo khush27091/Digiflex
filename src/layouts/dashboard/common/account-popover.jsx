26efbd55c3604c38f60db035b731e3e980ff19ab
khush2709 <patelkhush560@gmail.com>
2025-07-13 17:12:35 +0530

    account popover updated

--- src/layouts/dashboard/common/account-popover.jsx
+++ src/layouts/dashboard/common/account-popover.jsx
@@ -16,18 +16,18 @@ import { account } from 'src/_mock/account';
 // ----------------------------------------------------------------------
 
 const MENU_OPTIONS = [
-  {
-    label: 'Home',
-    icon: 'eva:home-fill',
-  },
-  {
-    label: 'Profile',
-    icon: 'eva:person-fill',
-  },
-  {
-    label: 'Settings',
-    icon: 'eva:settings-2-fill',
-  },
+  // {
+  //   label: 'Home',
+  //   icon: 'eva:home-fill',
+  // },
+  // {
+  //   label: 'Profile',
+  //   icon: 'eva:person-fill',
+  // },
+  // {
+  //   label: 'Settings',
+  //   icon: 'eva:settings-2-fill',
+  // },
 ];
 
 // ----------------------------------------------------------------------
